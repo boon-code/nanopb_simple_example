b0fae469290ec9f1d9bcaf4acb5d0ea05d038d02
Manuel Huber <manuel.h87@gmail.com>
2025-09-11 01:26:22 +0200

    Add missing parenthesis in macro

--- my_nanopb_config.h
+++ my_nanopb_config.h
@@ -3,5 +3,5 @@
 #define PB_SYSTEM_HEADER "my_system.h"
 #define PB_ENABLE_MALLOC 1
 
-#define pb_realloc(ptr, size) arena_realloc(ptr, size)
-#define pb_free(ptr) arena_free(ptr)
+#define pb_realloc(ptr, size) arena_realloc((ptr), (size))
+#define pb_free(ptr) arena_free((ptr))
